63ae32e74fcabf773619e817f19743aa07190739
Patric Eckhart <mail@patriceckhart.com>
2022-06-23 06:19:58 +0200

    run commands as www-data, optimize container

--- root-files/opt/custom.sh
+++ root-files/opt/custom.sh
@@ -4,6 +4,6 @@ if [ -f "/data/neos/startup.sh" ]; then
 
 	echo "Run custom startup.sh from repository ..."
 
-	su root -c "/data/neos/startup.sh"
+	sh /data/neos/startup.sh
 
 fi
--- root-files/opt/neos/provisioning.sh
+++ root-files/opt/neos/provisioning.sh
@@ -6,8 +6,7 @@ if [ ! -z ${RUN_DOCTRINE_MIGRATE+x} ]; then
 
 	echo "Migrate database ..."
 
-	su root -c "/root-files/opt/neos/cli/cli-doctrinemigrate.sh"
-	su root -c "/root-files/opt/neos/cli/cli-permissions.sh"
+	doctrinemigrate
 
 fi
 
@@ -15,8 +14,7 @@ if [ ! -z ${RUN_DOCTRINE_UPDATE+x} ]; then
 
 	echo "Update database ..."
 
-	su root -c "/root-files/opt/neos/cli/cli-doctrineupdate.sh"
-	su root -c "/root-files/opt/neos/cli/cli-permissions.sh"
+	doctrineupdate
 
 fi
 
@@ -24,24 +22,21 @@ if [ ! -z ${RUN_FLUSHCACHE+x} ]; then
 
 	echo "Flushing cache ..."
 
-	su root -c "/root-files/opt/neos/cli/cli-flushcache.sh"
-	su root -c "/root-files/opt/neos/cli/cli-permissions.sh"
+	flushcache
 
 fi
 
-if [ ! -z ${SITE_PACKAGE+x} ]; then 
+if [ ! -z ${SITE_PACKAGE+x} ]; then
 
 	if [ ! -e "$PROVISIONINGFILE" ]; then
 
 		echo "Provisioning Neos ..."
 
-		su root -c "/root-files/opt/neos/cli/cli-flushcache.sh"
+		flushcache
 
 		cd /data/neos && ./flow site:import --package-key ${SITE_PACKAGE}
 
 		touch /data/.provisioned
-
-		su root -c "/root-files/opt/neos/cli/cli-permissions.sh"
 
 	fi
 
